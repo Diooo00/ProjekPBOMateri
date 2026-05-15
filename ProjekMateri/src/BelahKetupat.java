65143d0dd5c3159b5d0686030c510b38ce3933fa
Diooo00 <dioputranto0@gmail.com>
2026-05-15 13:42:23 +0700

    Add files via upload

---
+++ ProjekMateri/src/BelahKetupat.java
@@ -0,0 +1,33 @@
+/**
+ * ENCAPSULATION + INHERITANCE
+ * Bangun dasar belah ketupat.
+ */
+public class BelahKetupat extends BangunRuang {
+    private double diagonal1;
+    private double diagonal2;
+
+    public BelahKetupat(double diagonal1, double diagonal2) {
+        this.diagonal1 = diagonal1;
+        this.diagonal2 = diagonal2;
+    }
+
+    public double getDiagonal1() { return diagonal1; }
+    public double getDiagonal2() { return diagonal2; }
+
+    public double hitungSisi() {
+        return Math.sqrt(Math.pow(diagonal1 / 2, 2) + Math.pow(diagonal2 / 2, 2));
+    }
+
+    public double hitungLuas() {
+        return (diagonal1 * diagonal2) / 2;
+    }
+
+    @Override public double hitungLuasPermukaan() { return hitungLuas(); }
+    @Override public double hitungVolume()         { return 0; }
+    @Override public String getNamaBangun()        { return "Belah Ketupat"; }
+
+    @Override
+    public String getRingkasan() {
+        return String.format("d1=%.1f d2=%.1f", diagonal1, diagonal2);
+    }
+}
